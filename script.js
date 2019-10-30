e53b5362114e7168ee9b47cf4ba60a1a14b42f8f
j.bogg <jonathan.bogg@gmail.com>
2019-10-30 17:29:25 +0100

    Set media queries, scnd page done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * Add JSDoc
  * .welcome-btn-con, .welcome-btn fadeOut and remove on click function
  */
-$('.welcome-btn-con').hover(function () {
+$(document.querySelectorAll('.boxshadow')).hover(function () {
     $(this).removeClass('boxshadow');
 }, function () {
     $(this).addClass('boxshadow');
@@ -34,26 +34,82 @@ function revealContent() {
     $(allDisplayNone).delay(2500).fadeIn(300, function () {
         $(allDisplayNone).add('display-flex');
     });
+
+    return createFormElements();
+
 }
 
 let theForm = document.getElementsByTagName('form');
-let inputName = document.getElementsByName('choice');
-
-inputName.forEach(loopChoices);
+let radioContent = document.getElementsByName('choice');
 
 /**
  * 
- * @param {*} item 
- * @param {*} index 
+ * @param {*} item input
+ * @param {number} index index för varje input
  */
-function loopChoices(item, index) {
-    choiceVal = item.value;
-    index; // Används ej.
+function createFormElements() {
+    radioContent.forEach(loopChoices);
 
-    let itemId = item.id;
-    let newLabel = document.createElement('label');
-    newLabel.setAttribute('for', itemId);
+    function loopChoices(item, index) {
+        choiceVal = item.value;
+        index; // Används ej.
 
-    newLabel.innerHTML += "" + choiceVal + "<br>";
-    item.after(newLabel);
+        let itemId = item.id;
+
+        let newLabel = document.createElement('label');
+
+        newLabel.setAttribute('for', itemId);
+        newLabel.innerHTML += "" + choiceVal;
+        item.after(newLabel);
+    }
+    return false;
+
+}
+
+let userChoices;
+let choiceCheck = 0;
+
+function radioValidation() {
+    const radioChoices = document.getElementsByName('choice');
+    let i = 0, len = radioChoices.length;
+    let checked = false;
+
+    for (; i < len; i++) {
+        if (radioChoices[i].checked) {
+            checked = true;
+            userChoices = radioChoices[i].value;
+        }
+    }
+
+    if (!checked) {
+        alert('Hoppsan! Välj något av alternativen för att gå vidare.');
+        return false;
+    }
+
+    if (userChoices === "Gör ingenting") {
+        console.log('Gör ingenting!');
+        youChose(userChoices);
+
+        choiceCheck = 0;
+
+        return false;
+    }
+
+    else if (userChoices === "Spring härifrån") {
+        console.log('Spring härifrån!');
+        youChose(userChoices);
+
+        choiceCheck = 1;
+
+        return false;
+    }
+}
+
+function youChose(choice) {
+    let oldText = document.querySelector('p');
+    let newText = document.createElement('p');
+    newText.classList.add('italic-font');
+    newText.innerHTML += '> ' + choice;
+
+    oldText.appendChild(newText);
 }
