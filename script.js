64885658a284406fe3a4afa1588a4b043e82faf3
j.bogg <jonathan.bogg@gmail.com>
2019-11-01 15:22:11 +0100

    --

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
  * Add JSDoc
  * .welcome-btn-con, .welcome-btn fadeOut and remove on click function
  */
+
+
 $(document.querySelectorAll('.boxshadow')).hover(function () {
 
     $(this).removeClass('boxshadow');
@@ -29,14 +31,21 @@ function fadeWelcome() {
 
 }
 
+let parentPathOne = document.querySelectorAll('.path-one, .submit-btn');
+let deadPathOne = document.querySelector('.question-one-dead');
 /**
  * 
  */
 function revealPathOne() {
 
-    let parentPathOne = document.querySelectorAll('.path-one, .submit-btn');
-    let deadPathOne = document.querySelector('.question-one-dead');
-    $(deadPathOne).hide();
     $(parentPathOne).delay(2500).fadeIn(300);
+    $(deadPathOne).hide();
 
 }
+
+let submitBtn = document.querySelector('.submit-btn');
+
+$(submitBtn).click(function () {
+    validatePathOne();
+    return false;
+});
--- story.js
+++ story.js
@@ -1,14 +1,84 @@
 let userChoices;
-let radioChoices = document.getElementsByTagName('input');
 let checked = false;
+let radioChoices = $("input[name='choice']");
 let choicesOne = document.querySelector('.choices-one');
-
+let parentPathTwo = document.querySelectorAll('.path-two');
+let parentPathThree = document.querySelectorAll('.path-three');
+let choicesTwo = document.querySelector('.choices-two');
+let deadPathTwoOne = document.querySelector('.question-two-dead-one');
+let restartBtn = document.querySelectorAll('.restart-btn-con, .restart-btn');
+/**
+ * 
+ */
 function notChecked() {
     alert('Hoppsan! Välj något av alternativen för att gå vidare.');
     return false;
 }
 
-function radioValidation() {
+function restartNow() {
+    $('.restart-btn-con, .restart-btn').show();
+}
+
+/**
+ * 
+ */
+function validatePathOne() {
+
+    // Loopa genom vägval
+    for (i = 0; i < radioChoices.length; i++) {
+        if (radioChoices[i].checked) {
+            checked = true;
+            userChoices = radioChoices[i].value;
+
+            $(choicesOne).hide();
+        }
+    }
+
+    // Inget val
+    if (!checked) {
+        notChecked();
+
+        return false;
+    }
+
+    // Vägval 1 -- Val 1
+    if (userChoices === "Gör ingenting") {
+        youChose(userChoices);
+        
+        $(choicesOne).hide();
+        $(deadPathOne).show();
+
+        $(restartBtn).show();
+        $(restartBtn).click(function () {
+            window.location.reload(true);
+        });
+
+        return false;
+    }
+
+    // Vägval 1 -- Val 2
+    else if (userChoices === "Spring därifrån") {
+        youChose(userChoices);
+
+        $(parentPathTwo).show();
+        
+        $('.question-two-dead-one, .question-two-dead-two').hide();
+
+        // Ersätt funktion för submitknapp vid klick
+        $(submitBtn).click(function () {
+            validatePathTwo();
+            return false;
+
+        });
+        return false;
+    }
+    return false;
+}
+
+
+function validatePathTwo() {
+
+    $(parentPathThree).show();
 
     for (i = 0; i < radioChoices.length; i++) {
         if (radioChoices[i].checked) {
@@ -23,56 +93,66 @@ function radioValidation() {
         return false;
     }
 
-    // Vägval 1 -- Val 1
-    if (userChoices === "Gör ingenting") {
+    // Vägval 2 -- Val 1
+    if (userChoices === "Samma underliga ljud") {
         youChose(userChoices);
-        $(deadPathOne).show();
-        $(choicesOne).hide();
 
-        return;
+        $(parentPathTwo).hide();
+        $(deadPathTwoOne).show();
+
+        return false;
+    }
+    // Vägval 2 -- Val 2
+    if (userChoices === "Skrapande ljud") {
+        youChose(userChoices);
+
+        $(parentPathTwo).hide();
+        $(dead)
+
+        $(choicesTwo).hide();
+        $('.question-two-dead').hide();
+
+        $(submitBtn).click(function () {
+            validatePathThree();
+            return false;
+        });
+    }
+    // Vägval 2 -- Val 3
+    else if (userChoices === "Jamande") {
+        youChose(userChoices);
+        return false;
+    }
+    return false;
+}
+
+function validatePathThree() {
+
+    for (i = 0; i < radioChoices.length; i++) {
+        if (radioChoices[i].checked) {
+            checked = true;
+            userChoices = radioChoices[i].value;
+        }
     }
 
-    // Vägval 1 -- Val 2
-    if (userChoices === "Spring därifrån") {
-        youChose(userChoices);
-
-        let parentPathTwo = document.querySelectorAll('.path-two');
-        $(parentPathTwo).show();
-        $(choicesOne).hide();
-        $('.question-two-dead-one, .question-two-dead-two').hide();
-
-        userChoices = "";
-        checked = false;
-        
-        for (let i = 0; i < radioChoices.length; i++) {
-            if (radioChoices[i].checked) {
-                checked = true;
-                userChoices = radioChoices[i].value;
-            }
-            console.log(userChoices);
-
-            // Vägval 2 -- Val 1
-            if (userChoices = "Samma underliga ljud") {
-                youChose(userChoices);
-
-            }
-            // Vägval 2 -- Val 2
-            if (userChoices === "Skrapande ljud") {
-                youChose(userChoices);
-                return false;
-            }
-            // Vägval 2 -- Val 3
-            else if (userChoices === "Jamande") {
-                youChose(userChoices);
-                return false;
-            }
-
-            return checked;
-        }
+    if (!checked) {
+        notChecked();
 
         return false;
     }
 
+    // Vägval 3 -- Val 1
+    if (userChoices === "Öppna lådan") {
+        youChose(userChoices);
+        alert('ONe');
+        return false;
+    }
+
+    // Vägval 3 -- Val 2
+    else if (userChoices === "Hallå där! Slut med detektivarbetet, din fikarast är över!") {
+        youChose(userChoices);
+        alert('two');
+        return false;
+    }
     return false;
 }
 
@@ -86,6 +166,9 @@ function youChose(theChoice) {
     newText.innerHTML = '> ' + theChoice; // add theChoice text to new <p>
 
     oldText.appendChild(newText); // Add newText after oldText
+
+    userChoices = "";
+    checked = false;
 
     return;
 }
