3df85a8bb0d688ff5d272f93de3d6cbb633ab584
j.bogg <jonathan.bogg@gmail.com>
2019-11-03 22:09:55 +0100

    finishing touches

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Laboration 1
+Uppgiften är att tolka fritt och skapa en s.k. "textbaserad interaktiv upplevelse där användaren kan göra förändringar i en värld".
 
 ## Beskrivning
 Detta är en enkel textbaserad interaktiv berättelse där användaren är med och väljer. Ett vägval presenteras, användaren gör sitt val och går vidare eller... dör. Vid varje misslyckat försök måste användaren börja om från början. Vid slutet kan användaren återigen börja om, ifall denne vill.
 
-## Länk till GitHub repot + demolänk (GitHub Pages)
+### Repo
+https://github.com/boggzer/lab1.git
+### Demo
+https://boggzer.github.io/lab1/
--- script.js
+++ script.js
@@ -1,137 +1,170 @@
-$(document).ready(function doThis() {
-   $('.startsida').delay(2500).fadeOut('slow')
-
-   return revealFirstPage();
-});
-
-function revealFirstPage() {
-   $('.allting').delay(3400).fadeIn('slow', function () {
-       $(this).removeClass('display-none');
-   })
-};
-
-let submitBtn = $('input[type="submit"]');
-
-submitBtn.click(function () {
-   return radioValidation();
-});
-
 let formOne = document.getElementsByClassName('form-one');
 let formTwo = document.getElementsByClassName('form-two');
 let formThree = document.getElementsByClassName('form-three');
-let userChoice;
+
+/**
+ * @argument {object} submitBtn all input elements of type 'submit', in a NodeList
+ */
+let submitBtn = $('input[type="submit"]');
+
+/**
+ * @argument {string} userChoice variable that contains the user's choice, sent to function for validating input
+ */
+let userChoice = "";
+
+/**
+ * @argument {boolean} checked false to make function begin
+ */
 let checked = false;
 
+/**
+ * @function doThis
+ */
+$(document).ready(function doThis() {
+    $('.welcome-page').delay(2500).fadeOut('slow')
+    return revealFirstPage();
+});
+
+console.log(document.getElementsByTagName('input'));
+
+/**
+ * Function that fades in the function with removal of a class
+ * 
+ */
+function revealFirstPage() {
+    $('main').delay(3400).fadeIn('slow', function () {
+        $(this).removeClass('display-none');
+    });
+};
+
+
+/** 
+ * @event MouseEvent
+ * @fires {{classname}}#{{eventName}}{{}}
+ */
+submitBtn.click(function () {
+    return radioValidation();
+});
+
+
+/**
+ * @class
+ */
 function radioValidation() {
 
-   let radioChoices = $("input[name='choice']");
+    let radioChoices = $("input[name='choice']");
+    /**
+     * Loop
+     * @function for 
+     * @param {number} i
+     }
+     */
+    for (let i = 0; i < radioChoices.length; i++) {
+        if (radioChoices[i].checked) {
+            userChoice = radioChoices[i].value;
+            checked = true;
+        };
+    };
 
-   for (let i = 0; i < radioChoices.length; i++) {
-       if (radioChoices[i].checked) {
-           userChoice = radioChoices[i].value;
-           checked = true;
-       }
-   }
+    /**
+     * 
+     */
+    if (!checked) {
+        alert('Välj för att gå vidare');
+        return false;
+    }
 
-   if (!checked) {
-       alert('Välj för att gå vidare');
-       return false;
-   }
+    if (userChoice === "You peek behind the door to the kitchen.") {
+        /**
+         * @argument {}
+         */
+        let underThis = $('p.path-one');
+        youChose(userChoice, underThis);
+        $('.path-one-death').delay(300).fadeIn('slow', function () {
+            $(this).removeClass('display-none');
+        })
+        $(formOne).remove();
+        return restart();
+    }
 
-   if (userChoice === "Val ett") {
-       let underThis = $('p.choice-one');
-       youChose(userChoice, underThis);
+    if (userChoice === "Whatever, it's probably nothing.") {
+        let underThis = $('p.path-one');
+        youChose(userChoice, underThis);
+        $(formOne).remove();
+        $('.path-two-con').delay(100).fadeIn('slow', function () {
+            $(this).removeClass('display-none');
+        });
+        return checked = false;
+    }
 
-       $('.sida-ett-slut').delay(300).fadeIn('slow', function () {
-           $(this).removeClass('display-none');
-       })
-       $(formOne).remove();
+    if (userChoice === "Kitty came back home from on the island, but kitty came on home without a name...") {
+        let underThis = $('p.path-two');
+        youChose(userChoice, underThis);
+        $(formTwo).remove();
+        $('.path-three-con').delay(300).fadeIn('slow', function () {
+            $(this).removeClass('display-none');
+        })
+        return checked = false;
+    }
 
-       return restart();
-   }
+    if (userChoice === "You grab the bottle labeled 'Holy Water' bought at the local April Fool's store.") {
+        let underThis = $('p.path-two');
+        youChose(userChoice, underThis);
+        $(formTwo).remove();
+        $('.path-two-death').delay(300).fadeIn('slow', function () {
+            $(this).removeClass('display-none');
+        })
+        return restart();
+    }
 
-   if (userChoice === "Val två") {
-       let underThis = $('p.choice-one');
-       youChose(userChoice, underThis);
+    if (userChoice === "You stay quiet.") {
+        let underThis = $('p.path-three');
+        youChose(userChoice, underThis);
+        $(formThree).remove();
+        $('.path-three-death').delay(300).fadeIn('slow', function () {
+            $(this).removeClass('display-none');
+        })
+        return restart();
+    }
 
-       $(formOne).remove();
-
-       $('.sida-tva').delay(100).fadeIn('slow', function () {
-           $(this).removeClass('display-none');
-       });
-
-       return false;
-   }
-
-   if (userChoice === "Val tre") {
-       let underThis = $('p.choice-two');
-       youChose(userChoice, underThis);
-
-       $(formTwo).remove();
-
-       $('.sida-tva-slut').delay(300).fadeIn('slow', function () {
-           $(this).removeClass('display-none');
-       })
-
-       return restart();
-   }
-
-   if (userChoice === "Val fyra") {
-       let underThis = $('p.choice-two');
-       youChose(userChoice, underThis);
-
-       $(formTwo).remove();
-
-       $('.sida-tre').delay(300).fadeIn('slow', function () {
-           $(this).removeClass('display-none');
-       })
-
-       return false;
-   }
-
-   if (userChoice === "Val fem") {
-       let underThis = $('p.choice-three');
-       youChose(userChoice, underThis);
-
-       $(formThree).remove();
-       
-       $('.sida-tre-slut').delay(300).fadeIn('slow', function () {
-           $(this).removeClass('display-none');
-       })
-
-       return restart();
-   }
-
-   else if (userChoice === "Val sex") {
-       let underThis = $('p.choice-three');
-       youChose(userChoice, underThis);
-       
-       $('.slut').delay(300).fadeIn('slow', function () {
-           $(this).removeClass('display-none');
-       })
-       
-       $(formThree).remove();
-
-       return restart();
-   }
+    else if (userChoice === "You sing a few songs with a shaky voice.") {
+        let underThis = $('p.path-three');
+        youChose(userChoice, underThis);
+        $('.the-end').delay(300).fadeIn('slow', function () {
+            $(this).removeClass('display-none');
+        })
+        $(formThree).remove();
+        return restart();
+    }
 }
+/**
+ * 
+ * @param {*} theChoice 
+ * @param {*} oldText 
+ */
 
-let allP = document.querySelectorAll('p');
-console.log(allP, allP.length);
-
+/**
+ * Retrieves and adds user's choice beneath previous question
+ * @param {string} theChoice the string content of the user's checked radio button
+ * @param {object} oldText the previous text which is always the previous path question, as a HTMLCollection/NodeList/Object
+ * @returns {boolean} false, for no redirecting
+ */
 function youChose(theChoice, oldText) {
-   let newText = document.createElement('p');
-   newText.appendChild(document.createTextNode(theChoice));
-   // let oldText = document.querySelector('p');
-   oldText.append(newText);
+    let newText = document.createElement('p');
+    newText.innerHTML += '> ';
+    newText.appendChild(document.createTextNode(theChoice));
+    newText.classList.add('italic');
+    oldText.append(newText);
+
+    return theChoice = null;
 }
 
 function restart() {
-   $('.restart').delay(300).fadeIn('slow', function () {
-       $(this).removeClass('display-none');
-       $('.restart').click(function () {
-           window.location.reload();
-       });
-   });
-
+    $('.restart').delay(300).fadeIn('slow', function () {
+        $(this).removeClass('display-none');
+        $('.restart').click(function () {
+            window.location.reload();
+        });
+    });
+    return userChoice = null;
 }
